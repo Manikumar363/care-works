43e7795a396da3b8dbafefe1fa12e44aae30a3a3
Manikumar363 <allemanikumar363@gmail.com>
2026-04-03 19:27:59 +0530

    redirected to signin

--- src/components/careProvider/IntroductionWithCTA.tsx
+++ src/components/careProvider/IntroductionWithCTA.tsx
@@ -69,7 +69,7 @@ const IntroductionWithCTA = () => {
         </div>
 
         <div className="max-w-3xl">
-          <Link href="https://caregiver.careworks.biz/signup" target="_blank" rel="noopener noreferrer">
+          <Link href="https://caregiver.careworks.biz/signin" target="_blank" rel="noopener noreferrer">
             <YellowButton className="px-14 py-7 text-xl">Register now</YellowButton>
           </Link>
         </div>
